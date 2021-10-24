ba36d72a8f83b778f4fd54cac8d669abb0df02d4
akashvadnala <akashvadnala123@gmail.com>
2021-10-24 11:54:19 +0530

    Commit changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn newsclub.wsgi --log-file -
--- newssite/settings.py
+++ newssite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-q_w^!7%tu%9yl0e8*t3i35=hv4&%^_yhofl-*)-j19-ugb_dr^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
