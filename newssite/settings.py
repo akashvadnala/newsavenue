d7de462776df11dcdd25bfeab9f17c56d15295c9
akashvadnala <akashvadnala123@gmail.com>
2021-10-25 09:14:06 +0530

    commit changes

--- newssite/settings.py
+++ newssite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-q_w^!7%tu%9yl0e8*t3i35=hv4&%^_yhofl-*)-j19-ugb_dr^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,13 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-'''STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
-]'''
+]
 
 MEDIA_URL = '/media/'
 
