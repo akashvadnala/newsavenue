64f0deee19a122c87d0b44005bc999c4be8b129c
akashvadnala <akashvadnala123@gmail.com>
2021-10-25 15:38:53 +0530

    Commit changes

--- newssite/settings.py
+++ newssite/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-'''
-STATICFILES_DIRS = [
+
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
-]
-'''
+]'''
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/img')
